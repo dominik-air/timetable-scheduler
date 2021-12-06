d130d8d2d3515b0cc4f08c64c5753d3dd090be87
keliasz <KEliasz145@gmail.com>
2021-12-06 17:20:33 +0100

    Update cost_functions.py

--- cost_functions.py
+++ cost_functions.py
@@ -9,7 +9,7 @@ s = np.array([[[0, 2, 3],
                [0, 4, 1]]])
 
 
-def gaps_c_function(x: np.ndarray, w1: float):
+def gaps_c_function(x: np.ndarray, w1: float) -> float:
     function_cost = 0
     ind = {}
     for n, day in enumerate(x):
@@ -36,5 +36,18 @@ def gaps_c_function(x: np.ndarray, w1: float):
     return function_cost
 
 
+def unbalanced_function(x: np.ndarray, w2: float) -> float:
+    cost_function = 0
+    H = np.count_nonzero(x)
+    for n, day in enumerate(x):
+        sum_group = 0
+        for group in range(day.shape[1]):
+            h = np.count_nonzero(day[:, group])
+            sum_group += np.abs(H/(len(x)*day.shape[1])-h)
+        cost_function += w2*sum_group
+    return cost_function
+
+
+# print(unbalanced_function(s, 1))
 # x = gaps_c_function(s, 2)
 # print(x)
