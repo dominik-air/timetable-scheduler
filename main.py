29407ad48b19fa1b94d660f2686e46492f17eff2
Arkadiusz Kontek <arek8131@op.pl>
2021-12-29 17:01:03 +0100

    gui_next_update

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import timetable_scheduler
 
 if __name__ == '__main__':
-    setup = timetable_scheduler.StatisticalTestsAlgorithmSetup(cooling_schedule=timetable_scheduler.simulated_annealing.exponential_cooling_schedule, alpha=0.9)
+    setup = timetable_scheduler.StatisticalTestsAlgorithmSetup(
+        cooling_schedule=timetable_scheduler.simulated_annealing.exponential_cooling_schedule, alpha=0.999)
     results = setup.SA()
     timetable_scheduler.export_matrix_to_excel(results.best_solution_matrix)
     # open the Result Schedule in Excel
