6e3073b8f406dcb7379d35cdaf7024e2b73e1ce9
dominik-air <62311964+dominik-air@users.noreply.github.com>
2022-01-10 23:21:39 +0100

    final project structure

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ if __name__ == '__main__':
     timetable_scheduler.create_dataset(term_id=5, lecturer_p=1, room_p=1)
     setup = timetable_scheduler.StatisticalTestsAlgorithmSetup(
         cooling_schedule=timetable_scheduler.simulated_annealing.exponential_cooling_schedule,
-        operator_probabilities=[0.1, 0.1, 0.8], Tmax=20, Tmin=1, kmax=100, alpha=0.999)
+        operator_probabilities=[0.33, 0.33, 0.34], Tmax=30, Tmin=0.00133, kmax=30, alpha=0.995)
+
     results = setup.SA()
 
     print(f'Time elapsed: {results.elapsed_time}')
@@ -14,3 +15,4 @@ if __name__ == '__main__':
     plt.plot(setup.f_costs, 'b--')
     plt.plot(setup.best_cost_change, 'r-')
     plt.show()
+
